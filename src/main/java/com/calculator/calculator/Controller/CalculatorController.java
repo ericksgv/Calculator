95b2f5370bda7dd1f4b38d0dcebb1a31f81526ca
Erick <sangaravi22@gmail.com>
2022-09-27 20:25:59 -0500

    Functional number and operation keys

--- src/main/java/com/calculator/calculator/Controller/CalculatorController.java
+++ src/main/java/com/calculator/calculator/Controller/CalculatorController.java
@@ -1,14 +1,19 @@
 package com.calculator.calculator.Controller;
 
+import com.calculator.calculator.Model.CalculatorModel;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
 
+import java.util.ArrayList;
+import java.util.List;
+
+
 public class CalculatorController {
 
     @FXML
-    private TextField result;
+    private TextField screen;
 
     @FXML
     private Button btnAddition;
@@ -64,50 +69,111 @@ public class CalculatorController {
     @FXML
     private Button btnZero;
 
-    @FXML
-    void clickClear(ActionEvent event) {
+    CalculatorModel calculatorModel;
 
+    void initialize(){
+        calculatorModel = new CalculatorModel();
     }
 
     @FXML
-    void clickEqual(ActionEvent event) {
-
+    void clickClear(ActionEvent event) {
+        if (screen.getText().isEmpty())
+            return;
+        screen.setText("");
     }
 
     @FXML
     void clickNumberButton(ActionEvent event) {
-        String number = getButtonValue(event);
-        String textResult = result.getText();
+        Button button = (Button)event.getSource();
+        String number = button.getText();
+        String textScreen = screen.getText();
+        char lastChar = '.';
+        if(!textScreen.isEmpty())
+            lastChar = textScreen.charAt(textScreen.length() - 1);
+
         if(".".equals(number)){
-            boolean contains = textResult.contains(".");
-            if(contains||isBlank(textResult)){
+            boolean contains = textScreen.contains(".");
+            if(screen.getText().isEmpty() || lastChar == '%' || lastChar == '+' || lastChar == '-' || lastChar == '*' || lastChar == '÷' || lastChar == '.'){
                 return;
             }else {
-                result.setText(textResult+".");
+                screen.setText(textScreen+".");
                 return;
             }
         }
-        result.setText(result.getText()+number);
+        screen.setText(screen.getText()+number);
     }
-
-    private String getButtonValue(ActionEvent event){
-        Button button = (Button)event.getSource();
-        return button.getText();
-    }
-
-
-    private boolean isBlank(String text){
-        if(text!=null&&text!=""&&text.length()>0)
-            return false;
-        return true;
-    }
-
-
-
-
 
     @FXML
     void clickOperationButton(ActionEvent event) {
+        Button button = (Button)event.getSource();
+        String operation = button.getText();
+        String textScreen = screen.getText();
+        char lastChar = '.';
+        if(!textScreen.isEmpty())
+            lastChar = textScreen.charAt(textScreen.length() - 1);
+
+        if(screen.getText().isEmpty())
+            return;
+        else if (lastChar == '.' || lastChar == '%' || lastChar == '+' || lastChar == '-' || lastChar == '*' || lastChar == '÷')
+            return;
+
+        screen.setText(textScreen+operation);
+
+        //screen.setText(screen.getText()+operation);
+    }
+
+    @FXML
+    void clickEqual(ActionEvent event) {
+        String textScreen = screen.getText();
+        char lastChar = '.';
+        String[]numRead = textScreen.split("%|\\+|\\-|\\*|\\÷");
+        Float[]numbers = null;
+        ArrayList<Character> operations = new ArrayList<>();
+
+        if(!textScreen.isEmpty())
+            lastChar = textScreen.charAt(textScreen.length() - 1);
+        else
+            return;
+        if (lastChar == '.' || lastChar == '%' || lastChar == '+' || lastChar == '-' || lastChar == '*' || lastChar == '÷')
+            return;
+        if(numRead != null){
+            numbers = new Float[numRead.length];
+            for (int i = 0; i < numRead.length; i++) {
+                numbers[i] = Float.parseFloat(numRead[i]);
+            }
+            int counter = 0;
+            for (int i = 0; i < textScreen.length(); i++)
+                if (textScreen.charAt(i) == '%' || textScreen.charAt(i) == '+' || textScreen.charAt(i) == '-' || textScreen.charAt(i) == '*' || textScreen.charAt(i) == '÷') {
+                    operations.add(textScreen.charAt(i));
+                    System.out.println("OpGuardada: " + operations.get(counter));
+                    counter++;
+                }
+            for(int i = 0; i < operations.size(); i++){
+                switch(operations.get(i)){
+                    case '+':
+                        screen.setText(String.valueOf(numbers[0] + numbers[1]));
+                        break;
+
+                    case '-':
+                        screen.setText(String.valueOf(numbers[0] - numbers[1]));
+                        break;
+
+                    case '%':
+                        screen.setText(String.valueOf(numbers[0] % numbers[1]));
+                        break;
+
+                    case '÷':
+                        screen.setText(String.valueOf(numbers[0] / numbers[1]));
+                        break;
+
+                    case '*':
+                        screen.setText(String.valueOf(numbers[0] * numbers[1]));
+                        break;
+                }
+            }
+        }
+
+
 
     }
 
---
+++ src/main/java/com/calculator/calculator/Model/CalculatorModel.java
@@ -0,0 +1,15 @@
+package com.calculator.calculator.Model;
+import java.util.Vector;
+
+public class CalculatorModel {
+
+    public Vector numberVect;
+
+    public CalculatorModel() {
+    }
+
+    public CalculatorModel(Vector numberVect) {
+        this.numberVect = numberVect;
+    }
+
+}
